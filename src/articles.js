0c013f17a8297a7a9d3293c846040d9c150863e3
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-03-12 21:10:04 +0100

    add articles data

---
+++ src/articles.js
@@ -0,0 +1,148 @@
+export default [
+    {
+        "id": 1488641244113,
+        "date": "2017-03-04T15:27:24.113Z",
+        "title": "Lorem ipsum",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempus erat ultricies congue pellentesque. Phasellus vel nibh non lectus feugiat consectetur quis et magna. Etiam iaculis magna neque. Donec leo magna, viverra ut imperdiet non, rhoncus id enim. Morbi ut risus at purus tincidunt dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia faucibus quam, ac facilisis nibh lobortis id. Proin feugiat interdum neque, in commodo mauris fermentum eget. Praesent nec tortor eu est porttitor scelerisque. Nulla vulputate neque nec nibh dictum, ut sollicitudin justo placerat. Maecenas in ex in sapien sollicitudin facilisis.",
+        "comments": [
+            {
+                "id": 1488641244110,
+                "user": "Roselyn Liang",
+                "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus vestibulum arcu, quis accumsan ipsum finibus a. Phasellus sagittis eros id ipsum tincidunt, non imperdiet enim vestibulum. Pellentesque cursus enim et lacus pellentesque auctor. Nam non lacus leo. Phasellus diam lectus, tristique et tellus non, consectetur placerat mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus felis lectus, vehicula nec porttitor a, porta sed justo. Aliquam pretium lorem ut ante laoreet, in eleifend risus vulputate. Maecenas ligula ligula, sodales et leo nec, venenatis tincidunt massa. Nulla commodo scelerisque tempor."
+            },
+            {
+                "id": 1482641244113,
+                "user": "Rosaura Bickley",
+                "text": "Phasellus urna nibh, bibendum tempor iaculis et, commodo eget lectus. Cras id ante facilisis ex posuere sodales. Nulla ultrices nisl ac pulvinar pellentesque. Quisque bibendum facilisis est, id scelerisque lectus porttitor ac. Sed risus tellus, sollicitudin ac ligula nec, porta pellentesque augue. Nam a risus orci. Vivamus malesuada molestie turpis, posuere congue risus commodo facilisis. Vivamus iaculis congue venenatis. Integer nec eleifend quam, vel aliquam magna. Ut mi ligula, consectetur hendrerit accumsan id, egestas at est."
+            }
+        ]
+    },
+    {
+        "id": 1488641244114,
+        "date": "2017-03-05T15:27:24.113Z",
+        "title": "Orci varius",
+        "text": "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer eget dui pellentesque, rutrum arcu quis, malesuada elit. Sed viverra, velit pellentesque molestie fringilla, dui quam faucibus erat, eu luctus felis orci rutrum dui. Pellentesque arcu massa, volutpat a malesuada eget, gravida non elit. Morbi porta tristique rhoncus. Nam odio tellus, dapibus cursus est quis, laoreet congue erat. Maecenas malesuada nisi ante, ullamcorper lacinia magna iaculis a. Suspendisse scelerisque vulputate eros, quis tempus ante lacinia non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et efficitur orci. Suspendisse potenti. Donec venenatis, metus sit amet rhoncus pulvinar, libero mi gravida turpis, quis dictum velit tellus sed urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam eu turpis sed dolor blandit varius eu non lectus.",
+        "comments": [
+            {
+                "id": 1488642224113,
+                "user": "Maureen Purser",
+                "text": "Proin dictum lectus magna, eget pulvinar tellus ultrices quis. Phasellus gravida faucibus urna, at laoreet ipsum tristique in. Proin gravida, tellus vel pellentesque consequat, eros lacus eleifend urna, eu mattis massa turpis ut nulla. Curabitur nec dignissim nisi. Donec rhoncus massa id purus rutrum semper. Vivamus nec ante posuere, dapibus enim in, iaculis nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac risus quis nisi lobortis consequat. Fusce feugiat ipsum a diam fermentum, eget convallis augue mattis. Duis imperdiet eu ligula at molestie. Nullam mollis nunc nec ornare iaculis. Etiam nec tristique nulla, eget vestibulum libero. Aliquam erat volutpat. Suspendisse lectus ante, dapibus vel tempus in, volutpat eget dui."
+            },
+            {
+                "id": 1488641244120,
+                "user": "Rubye Bast",
+                "text": "Cras faucibus fringilla malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer porttitor lorem vel odio mollis ullamcorper. Pellentesque eget justo nisi. Fusce sagittis accumsan turpis, nec sollicitudin tortor mattis in. Praesent bibendum ornare dapibus. Vivamus mollis tortor eu velit tempus, id auctor felis vestibulum. Nulla fringilla lectus sit amet molestie posuere. Curabitur sit amet gravida ipsum. Quisque interdum lectus sed sapien pretium viverra."
+            },
+            {
+                "id": 1488641164113,
+                "user": "Susie Bensinger",
+                "text": "Fusce cursus, erat sed vestibulum egestas, sem nulla aliquam libero, eu aliquet ipsum lectus eu nunc. Aenean elementum eros vel urna fermentum, luctus eleifend mi blandit. Fusce sem libero, facilisis eu mattis non, malesuada vitae magna. Maecenas molestie, quam id ultricies maximus, massa dolor sagittis tellus, in vestibulum purus sapien id ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum porttitor iaculis. Proin tellus neque, sodales sed auctor vel, cursus ac orci. Suspendisse accumsan finibus tincidunt. Aenean luctus quis neque sed convallis. Mauris eleifend gravida scelerisque. Quisque sapien mauris, pellentesque a lorem vitae, hendrerit feugiat dolor. Proin euismod metus a purus laoreet rhoncus. Suspendisse potenti."
+            },
+            {
+                "id": 1488641244223,
+                "user": "Willis Massi",
+                "text": "Donec eu purus arcu. Praesent porta diam et dolor semper convallis. Phasellus tristique augue quam, ac interdum ligula porta eu. Nullam id libero nec nibh aliquam euismod. Vestibulum lorem dolor, imperdiet sit amet vulputate malesuada, aliquam in nisi. Nunc a sem diam. Donec non consectetur velit. Vestibulum rhoncus ipsum sed porta volutpat. Nam scelerisque dui vel lacus luctus faucibus."
+            },
+            {
+                "id": 1488641764113,
+                "user": "Hanh Darden",
+                "text": "Vivamus justo diam, pretium sed lectus quis, pretium pretium dui. Aliquam nulla nibh, venenatis eu fermentum et, luctus ut sem. Aliquam erat volutpat. Pellentesque a dapibus erat. Suspendisse cursus augue mauris, id ornare risus condimentum ac. Aenean dui velit, sagittis ac velit in, dapibus blandit est. Nulla facilisi. Integer odio ex, blandit ut magna id, ullamcorper eleifend mauris. Nam vitae commodo nisl. Morbi id massa sollicitudin, vestibulum est et, dictum mauris. Curabitur hendrerit non nisl nec elementum. Donec sed ornare dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus mollis ex quis fermentum tempus."
+            }
+        ]
+    },
+    {
+        "id": 1488641244116,
+        "date": "2017-03-06T15:27:24.113Z",
+        "title": "Cras pellentesque",
+        "text": "Cras pellentesque ex a euismod rhoncus. Pellentesque pulvinar turpis luctus odio scelerisque fermentum. Fusce vel justo at quam commodo ultrices. Vestibulum sit amet lacinia sapien, quis ullamcorper elit. Sed nibh tortor, vulputate eu sodales et, sodales sed ex. Vivamus odio magna, gravida vel justo non, imperdiet consequat arcu. Fusce volutpat, odio vel vulputate porta, magna risus vestibulum dolor, at scelerisque mi mauris eu metus. Integer et varius eros. Vestibulum ullamcorper ultricies quam. Mauris tempus scelerisque velit ac aliquam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut sodales neque ligula, vitae maximus augue luctus vel. Morbi vel erat id lorem ornare fermentum nec sed odio. Morbi ultrices magna elementum ante aliquet, scelerisque efficitur ante condimentum. Pellentesque sed purus lobortis, ornare nisl eu, facilisis neque. Aliquam nibh odio, vestibulum eget molestie at, lobortis at felis.",
+        "comments": [
+            {
+                "id": 1488642234113,
+                "user": "Mandi Riess",
+                "text": "Cras ut volutpat urna. Sed dictum, turpis id posuere cursus, massa risus blandit nunc, ac dictum metus metus non risus. Nunc mattis urna quis metus auctor, facilisis ultrices ante rhoncus. Sed vestibulum nisl tempor, gravida sapien vitae, tincidunt est. Phasellus semper risus enim, eu placerat urna volutpat vitae. Fusce sed mauris urna. Morbi dolor libero, tristique sit amet porta vestibulum, vulputate et arcu. Donec aliquet tellus in scelerisque lobortis. Cras non velit ullamcorper, facilisis odio vitae, luctus ex. Sed pharetra justo tristique, gravida elit facilisis, semper arcu."
+            }
+        ]
+    },
+    {
+        "id": 1488641244118,
+        "date": "2017-03-07T15:27:24.113Z",
+        "title": "Integer venenatis",
+        "text": "Integer venenatis mollis dui, vel malesuada quam auctor nec. Vestibulum rutrum diam lectus, interdum efficitur arcu tempus id. Curabitur non diam ut arcu scelerisque dignissim id non quam. Sed sed ornare dui. Morbi eget ipsum quis massa efficitur maximus ut vitae lacus. Pellentesque nec porttitor ligula. Praesent dui risus, pulvinar ac accumsan sed, fringilla sed enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In vehicula vehicula tortor sed aliquet. Pellentesque vestibulum sapien pellentesque sapien efficitur, quis malesuada risus feugiat.",
+        "comments": [
+            {
+                "id": 1418641141113,
+                "user": "Elizabet Butz",
+                "text": "Integer ultrices neque et libero luctus dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin faucibus posuere purus eget convallis. Curabitur ex nulla, egestas ut finibus at, fringilla et velit. In rutrum tellus neque, sit amet scelerisque libero pretium nec. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam eleifend accumsan neque, eget congue sem ultricies sed. Fusce dignissim cursus nibh ut porttitor. Praesent ut iaculis lacus. Donec euismod lorem sit amet elementum sollicitudin."
+            },
+            {
+                "id": 1411641244113,
+                "user": "Lorretta Funston",
+                "text": "In a tempor dolor, vel facilisis felis. Pellentesque tincidunt bibendum massa, feugiat consectetur sem molestie nec. Vestibulum auctor tortor at diam blandit volutpat. Quisque in tempus tellus, a feugiat libero. Cras maximus diam lectus, egestas dapibus urna rhoncus eu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer blandit sapien pretium lobortis vulputate. Sed dictum quis ipsum vel imperdiet. Fusce at ligula quis mi dapibus fermentum ut vitae lectus. Sed id ligula sed dui euismod elementum. Nulla pretium in diam nec convallis. Nullam tempor eros id eros blandit, eget tristique massa iaculis. Ut euismod nulla nec massa pharetra egestas. Suspendisse feugiat ullamcorper metus in blandit. Sed quis justo porttitor, rutrum risus vel, rhoncus neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+            }
+        ]
+    },
+    {
+        "id": 1488611244113,
+        "date": "2017-03-08T15:27:24.113Z",
+        "title": "Duis vulputate",
+        "text": "Duis vulputate, lectus in molestie aliquet, ligula ante accumsan nisl, ac accumsan mauris quam non arcu. Sed semper nisl nec turpis maximus, eu euismod massa ornare. Vivamus finibus quis lorem a efficitur. Maecenas tristique est justo, quis dapibus erat venenatis sed. In at purus tempus, mattis est ut, egestas ipsum. Sed a dolor ut neque blandit aliquet eu a purus. Etiam quis nisi a dui tristique sagittis. Proin euismod purus justo, eget lobortis lectus facilisis sed. Phasellus eu erat nibh. Morbi tempor bibendum augue, sed placerat felis lacinia eget. Quisque non sapien nisl. Quisque vitae rhoncus libero. Donec nec lectus vel sem scelerisque bibendum. Donec egestas molestie ipsum non fringilla.",
+        "comments": [
+            {
+                "id": 1422641244113,
+                "user": "Yelena Migliore",
+                "text": "Morbi tristique purus vel eros rhoncus, ut cursus lectus consectetur. Fusce ut dignissim neque. Donec a rutrum risus, id pretium est. Etiam neque ligula, scelerisque nec consequat dictum, aliquet ut nibh. Quisque posuere lobortis ipsum sed aliquam. Vestibulum porttitor euismod dolor at fermentum. Fusce hendrerit ipsum nisi, vitae tempus diam facilisis a. Fusce placerat massa ex, ac congue ipsum ullamcorper eu."
+            }
+        ]
+    },
+    {
+        "id": 1418641244113,
+        "date": "2017-03-09T15:27:24.113Z",
+        "title": "Sed rutrum",
+        "text": "Sed rutrum, arcu ut volutpat ultrices, urna velit posuere nunc, vitae mollis nisl ipsum eu metus. Donec vel ornare urna. Sed et molestie sapien. Mauris vel nunc quis dui sagittis blandit maximus vel tellus. Vivamus dictum finibus nibh quis dapibus. Sed tempor augue at semper molestie. Nullam sit amet nisi porta, facilisis metus sit amet, sollicitudin massa. Mauris ut magna vitae orci commodo pretium non et magna. Donec cursus faucibus porttitor. Duis luctus suscipit ligula, vitae hendrerit felis efficitur rutrum.",
+        "comments": [
+            {
+                "id": 1444641244113,
+                "user": "Rueben Knabe",
+                "text": "Sed ac iaculis mauris, nec aliquam neque. Duis sit amet suscipit lorem. Sed porta congue odio, at iaculis turpis commodo et. Pellentesque facilisis at lectus quis vulputate. Vivamus vel ornare ligula. Ut in interdum metus, quis aliquam odio. Mauris in blandit dolor, ut sollicitudin lectus. Ut bibendum purus non ligula vehicula tincidunt. Etiam vitae erat eu massa porttitor finibus. Sed in ultricies libero. Maecenas quis diam orci. Vivamus venenatis fermentum eros, vitae efficitur nulla ornare ut."
+            },
+            {
+                "id": 1488641223113,
+                "user": "Slyvia Mead",
+                "text": "Morbi lectus neque, consequat sed vulputate ac, ornare interdum leo. Donec tempus nunc non aliquet feugiat. Aliquam mollis libero ut elit aliquet pellentesque. Nulla facilisi. Aenean eu tempus massa, vel aliquam nunc. Vestibulum commodo finibus magna, at rhoncus mauris scelerisque in. Etiam scelerisque vitae augue et egestas. Ut ut consectetur tellus. Mauris venenatis elit dui, eget iaculis mi consectetur vel. Fusce interdum facilisis orci vitae tincidunt. Fusce quis metus sem. Aenean id turpis et massa posuere consectetur. Aenean tempor purus eu erat vestibulum rhoncus. Nulla tincidunt arcu viverra dui aliquam varius. Sed et ex non augue egestas venenatis ac quis sapien. Praesent eget tempor dui."
+            },
+            {
+                "id": 1481241244113,
+                "user": "Jed Ovellette",
+                "text": "Donec sit amet gravida felis. Integer tempus laoreet metus eu elementum. Suspendisse imperdiet, augue ac iaculis iaculis, nunc tortor tempor orci, ut laoreet eros metus nec arcu. Duis mattis nisi ac sem efficitur fermentum. Sed id porta lorem. Pellentesque quis ultrices lorem. Aenean augue turpis, consectetur nec bibendum in, finibus vel tellus. Sed ultricies libero volutpat, laoreet erat id, imperdiet diam. Nunc in diam vitae nunc pulvinar mollis non at lorem. Praesent non elit ante. Vestibulum blandit, lectus hendrerit maximus volutpat, ante odio venenatis velit, nec viverra dui lectus eu orci. Fusce commodo enim eget purus dapibus, a porttitor velit volutpat. Ut id turpis gravida, scelerisque libero et, efficitur diam. Suspendisse metus diam, molestie nec nisl sit amet, iaculis fermentum leo."
+            }
+        ]
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
