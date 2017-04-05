3cadd8e090ffcdaf3936312ee8322bcbcf2f3c00
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-04-05 16:49:58 +0200

    create first component

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('Hello React!!!');
+import React from 'react';
+import { render } from 'react-dom';
+
+function App() {
+    return (
+        <h1>Hello World</h1>
+    );
+}
+
+render(
+    <App />,
+    document.getElementById('container')
+);
